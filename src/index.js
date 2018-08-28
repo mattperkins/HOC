0ff7ebfee104165e0f76ab3cf1bc15c08fbc6372
mattperkins <github@mattperkins.com>
2018-08-28 16:10:47 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import registerServiceWorker from './registerServiceWorker'
+// Modest Grid linked via public/index.html
+import './css/reset.css'
+import './css/splash.css'
+
+// main ("ROOT") component 
+class Root extends React.Component {
+
+render() {
+
+return (
+
+<App />
+
+)// end return
+}// end render
+}// end component
+
+ReactDOM.render(<Root />, 
+  document.getElementById('root'))
+  registerServiceWorker()
